9bbe349e77cb20561ea7ec26ea15b445bda370ed
Sung-Cheol Kim <sungcheol.kim78@gmail.com>
2023-01-22 08:53:22 -0500

    add time and clean up

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,26 @@ struct Walker {
     x: f64,
     y: f64,
     z: f64,
+    t: f64,
 }
 
 impl Walker {
-    fn next<R: Rng + ?Sized>(&mut self, rng: &mut R) {
-        let delta: f64 = rng.gen();
-        self.x += delta;
-        let delta: f64 = rng.gen();
-        self.y += delta;
-        let delta: f64 = rng.gen();
-        self.z += delta;
+    fn next<R: Rng + ?Sized>(&mut self, dt: f64, rng: &mut R) {
+        self.x += rng.gen::<f64>();
+        self.y += rng.gen::<f64>();
+        self.z += rng.gen::<f64>();
+        self.t += dt;
     }
 }
 
 fn main() {
     let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
-    let mut w = Walker { x:0.0, y:0.0, z:0.0 };
+    let mut w = Walker { x:0.0, y:0.0, z:0.0, t:0.0 };
 
     println!("Random Walker Simulator");
 
     for _i in 0..10 {
-        w.next(&mut rng);
+        w.next(0.1f64, &mut rng);
         println!("{:?}", w);
     }
 }
